c913726e18d29f06762fe1443de754bb28ad52e8
CeeSinc <131812058+CeeSinc@users.noreply.github.com>
2023-05-03 21:30:11 +0100

    Update TestNormalTraffic.py

--- TestNormalTraffic.py
+++ TestNormalTraffic.py
@@ -12,15 +12,15 @@ target_port = 80
 ip = IP(dst=target_ip)
 
 # Define a list of random ranges
-random_ranges = [(0, 0.1),(0, 0.01)]
+random_ranges = [(0, 0.002),(0.001, 0.004),(0, 0.01),(0.005, 0.02),(0.01, 0.04),(0.02, 0.05),(0.04, 0.06), (0.05, 0.08),(0.07, 0.1)]
 
 while True:
-    # Get current time in seconds since epoch 
+    # Get current time since start
     start_time = time.time()
-    # Generate a random duration in seconds to run loop 
-    duration_seconds = random.randint(2,10) 
+    duration_seconds = 1  
     # Randomly select range from list 
     rand_range=random.choice(random_ranges)
+    
     while True:
         # Check if duration has passed 
         if (time.time() - start_time) > duration_seconds:
@@ -28,9 +28,7 @@ while True:
     
         # Create TCP SYN packet with target port as destination port 
         SYN=TCP(sport=RandShort(), dport=target_port, flags="S", seq=42)
-    
-        # Send SYN packet without waiting for response 
+        # Send SYN packet 
         send(ip/SYN)
-    
         # Wait for rand amount before sending next packet 
         time.sleep(random.uniform(*rand_range))
